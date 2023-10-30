810d03fd31c06d3cc5663f2b18eaadf6161624cb
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-30 15:43:00 -0300

    feat: add navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="border-2 border-red-500 flex-between max-container padding-container relative z-30 py-5">
+        <Link href="/">
+            
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
