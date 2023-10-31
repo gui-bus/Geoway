71aec2bb22f9747d18c443811c7613619beb9319
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-31 18:02:03 -0300

    style: change button style

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -6,6 +6,7 @@ type ButtonProps = {
   icon_start?: ReactNode | string;
   icon_end?: ReactNode | string;
   variant?: string;
+  full?: boolean;
 };
 
 const Button = ({
@@ -14,11 +15,12 @@ const Button = ({
   icon_start,
   icon_end,
   variant,
+  full
 }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
     >
       {icon_start && icon_start}
       <label className="bold-16 whitespace-nowrap cursor-pointer">
