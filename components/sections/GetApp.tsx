b37284288e87b97b7579030afb2ee40ee3bd4503
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-31 20:16:34 -0300

    style: change how the phones are displayed on desktop

--- components/sections/GetApp.tsx
+++ components/sections/GetApp.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const GetApp = () => {
   return (
-    <section className="flexCenter w-full flex-col pb-28 mt-10">
+    <section className="flexCenter w-full flex-col pb-28 xl:pb-52 mt-10 xl:mt-28 relative">
       <div className="get-app">
         <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
           <h2 className="bold-40 l:bold-64 xl:max-w-[320px]">
@@ -30,10 +30,12 @@ const GetApp = () => {
             />
           </div>
         </div>
-
-        <div className="flex flex-1 items-center justify-end">
+        <div className="flex flex-1 items-center justify-end visible xl:invisible">
           <Image src="/phones.png" alt="Phones" width={550} height={870} />
         </div>
+      </div>
+      <div className="xl:flex flex-1 items-center justify-end absolute right-10 z-50 hidden">
+        <Image src="/phones.png" alt="Phones" width={550} height={870} />
       </div>
     </section>
   );
