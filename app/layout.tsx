8701e54f8363b315dcea7d4c3dfe8dcbe1d9b0ac
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-31 21:05:31 -0300

    feat: add seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -19,6 +20,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
+      <Head>
+        {/* Tags de SEO */}
+        <title>{metadata.title as string}</title>
+        <meta name="description" content={metadata.description as string} />
+        <meta name="keywords" content="Geoway, aventuras, explorar, mapas, rotas, trilhas, viagens" />
+        <meta name="author" content="Guilherme Bustamante" />
+        <link rel="canonical" href="https://geoway.vercel.app/" /> 
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content={metadata.title as string} />
+        <meta property="og:description" content={metadata.description as string} />
+        <meta property="og:image" content="/logo.png" />
+        <meta property="og:url" content="https://geoway.vercel.app/" />
+        <link rel="icon" type="image/png" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body className={montserrat.className}>
         <Navbar />
         <main className="relative overflow-hidden">{children}</main>
